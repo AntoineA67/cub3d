966e0f864b718dd5b9b88fef4de978a5c0f1beda
Qroussea <quentin42.rousseau@gmail.com>
2022-06-07 16:55:11 +0200

    jsp button

--- src/button.c
+++ src/button.c
@@ -111,7 +111,7 @@ t_coords screen_pc(double off, double wh, t_rgb colore, t_vars *vars)
 	res.a.x = vars->win_size.x * off_l / 100;
 	res.a.y = vars->win_size.y * off_d / 100;
 	res.b.x = res.a.x + (vars->win_size.x * width / 100);
-	res.b.y = res.a.y + (vars->win_size.x * height / 100);
+	res.b.y = res.a.y + (vars->win_size.y * height / 100);
 	res.a.c = colore;
 	res.vars = vars;
 	//printf("%d|%d|%d\n", res.x, res.y, res.z);
@@ -131,11 +131,11 @@ void	button(t_coords p, char *txt,void (*f)(void*, int), int data)
 	while (++dy < p.b.y)
 	{
 		dx = p.a.x;
-		while (++dx < p.b.x && dy - p.a.y < 95)
+		while (++dx < p.b.x)// && dy - p.a.y < 95)
 		{
 			if (txt)
 				add =  *(unsigned int *)(text->addr + ((int)(((dx - p.a.x) * text->size.x) / (p.b.x - p.a.x))
-							* (text->bits_per_pixel / 8) + ((int)(((dy - p.a.y) * text->size.y) / (p.b.y - p.a.y)) * text->line_length)));
+							* (text->bits_per_pixel / 8) + ((int)floor(((dy - p.a.y) * text->size.y) / (p.b.y - p.a.y)) * text->line_length)));
 			else
 				add = to_rgb(p.a.c, 0);
 			if (p.vars->clicked && p.vars->clicked_co.x >= p.a.x && p.vars->clicked_co.x <= p.b.x && p.vars->clicked_co.y >= p.a.y && p.vars->clicked_co.y <= p.b.y)
